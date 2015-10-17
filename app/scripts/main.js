e2403e87d0975b363fc31c91f653b050637cdc8d
Maciej Lato <mlato@brightroll.com>
2015-10-17 10:50:21 -0700

    added endorsements for pat

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,12 @@
-// jshint devel:true
-console.log('\'Allo \'Allo!');
+$(document).ready(function () {
+  // reload music players
+  $('a.reload-music').on('shown.bs.tab', function (e) {
+    if ($(this).hasClass('reload-music')) {
+      var players = $('#bandcamp_player');
+      $.each(players, function (idx) {
+        players[idx].src = players[idx].src;
+      });
+    }
+    $(this).removeClass('reload-music');
+  })
+});
