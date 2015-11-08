c675a9237ce4f89d536d66b0b6d82683e229f0d3
maciek <maciejplato@gmail.com>
2015-11-07 21:03:56 -0800

    images updated, double header for now

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -9,4 +9,21 @@ $(document).ready(function () {
     }
     $(this).removeClass('reload-music');
   })
+
+  //open/close primary navigation
+  $('.cd-primary-nav-trigger').on('click', function(){
+    $('.cd-menu-icon').toggleClass('is-clicked'); 
+    $('.cd-header').toggleClass('menu-is-open');
+    
+    //in firefox transitions break when parent overflow is changed, so we need to wait for the end of the trasition to give the body an overflow hidden
+    if( $('.cd-primary-nav').hasClass('is-visible') ) {
+      $('.cd-primary-nav').removeClass('is-visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend',function(){
+        $('body').removeClass('overflow-hidden');
+      });
+    } else {
+      $('.cd-primary-nav').addClass('is-visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend',function(){
+        $('body').addClass('overflow-hidden');
+      }); 
+    }
+  });
 });
