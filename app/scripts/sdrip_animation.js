19d229010cc85bd4aa90b3415975dbdb0244574b
maciek <maciejplato@gmail.com>
2016-07-26 01:23:08 -0700

    draft2 working, added some images. todo: fix animation curve

--- app/scripts/sdrip_animation.js
+++ app/scripts/sdrip_animation.js
@@ -3,9 +3,23 @@ $(document).ready(function() {
 	// 	$("#cf2 img.top").toggleClass("transparent");
 	// });
 	var controller = new ScrollMagic.Controller();
-	var scene = new ScrollMagic.Scene({triggerElement: "#section1"})
-		// trigger animation by adding a css class
-		.setClassToggle("#img-2", "fade")
-		.addIndicators({name: "1 - add a class"}) // add indicators (requires plugin)
-		.addTo(controller);
+
+	// var tween = TweenMax.to("#img-2", 12000, {css: {opacity: 0}});
+
+	var scene = new ScrollMagic.Scene({triggerElement: "#trigger1", duration: 2000, triggerHook: "onLeave"})
+								// .setTween("#img-2", {css: {opacity: 0}})
+								.setPin("#section-1", {pushFollowers: true})
+								.addIndicators({name: "1 (duration: 12000)"}) // add indicators (requires plugin)
+								.addTo(controller);
+	// debugger
+	// var scene2 = new ScrollMagic.Scene({triggerElement: "#trigger1", duration: 12000})
+	// 							.setTween("#img-2", {opacity: 0})
+	// 							// .triggerHook("onLeave")
+	// 							.addIndicators({name: "1 (duration: 12000)"}) // add indicators (requires plugin)
+	// 							.addTo(controller);
+	// // var scene = new ScrollMagic.Scene({triggerElement: "#trigger2"})
+	// // 							.setPin("#section-2")
+	// // 							.triggerHook("onLeave")
+	// // 							.addIndicators({name: "2 (duration: 500)"}) // add indicators (requires plugin)
+	// // 							.addTo(controller);
 	});
